75d53c46438a59c9cb4f068fbc6c827c71baf773
Dan Frenette <paradox815@gmail.com>
2015-05-24 15:14:22 -0400

    Add user authentication with monban

---
+++ db/migrate/20150524191058_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.integer :age, null: false
+      t.datetime :birthday, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
