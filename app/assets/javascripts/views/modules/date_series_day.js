a0a8b0213ff5f8ab8c50f60a79da74468feb4557
Dan Frenette <paradox815@gmail.com>
2015-07-27 21:43:12 -0400

    Add amCharts serial structure
    Added the AmCharts serial js file, which is used to make line and
    bar/column type graphs. Also added the structure for a daily date series
    graph. This commit is the general preparation for a specific type of
    AmCharts graph.

---
+++ app/assets/javascripts/views/modules/date_series_day.js
@@ -0,0 +1,49 @@
+MorningRoutine.Views.Modules.DateSeriesDay = function(el, graphData) {
+  this.$el = $(el);
+  this.id = this.$el[0].id;
+  this.legendData = legendData;
+  this.graphData = graphData;
+  this.graph = AmCharts.makeChart(this.id,
+      {
+        "type": "serial",
+        "categoryField": "date",
+        "dataDateFormat": "MM/DD/YYYY",
+        "pathToImages": "http://cdn.amcharts.com/lib/3/images/",
+        "categoryAxis": {
+          "parseDates": true
+        },
+        "chartCursor": {},
+        "chartScrollbar": {},
+        "trendLines": [],
+        "graphs": [
+        {
+          "bullet": "round",
+          "id": "AmGraph-1",
+          "title": "Word Count",
+          "valueField": "column-1"
+        }
+        ],
+        "guides": [],
+        "valueAxes": [
+        {
+          "id": "ValueAxis-1",
+          "title": "Axis title"
+        }
+        ],
+        "allLabels": [],
+        "balloon": {},
+        "legend": {
+          "useGraphSettings": true
+        },
+        "titles": [
+        {
+          "id": "journal-graph-title",
+          "size": 15,
+          "text": "Word Count by Day"
+        }
+        ],
+        "dataProvider": graphData
+      }
+  );
+};
+
